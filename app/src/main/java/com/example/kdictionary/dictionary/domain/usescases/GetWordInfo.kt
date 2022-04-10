b32214a9102992354a209a8115a2e5d7c1b4c3f3
Kenneth Ngedo <kenneth.ngedo@gmail.com>
2022-04-10 02:42:56 +0100

    feat: update use case

--- app/src/main/java/com/example/kdictionary/dictionary/domain/usescases/GetWordInfo.kt
+++ app/src/main/java/com/example/kdictionary/dictionary/domain/usescases/GetWordInfo.kt
@@ -1,6 +1,6 @@
 package com.example.kdictionary.dictionary.domain.usescases
 
-import com.example.kdictionary.core.util.Resource
+import com.example.kdictionary.commons.utils.Resource
 import com.example.kdictionary.dictionary.domain.models.WordInfo
 import com.example.kdictionary.dictionary.domain.repository.DictionaryRepository
 import kotlinx.coroutines.flow.Flow
